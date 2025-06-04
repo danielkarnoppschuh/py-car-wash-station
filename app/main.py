41da177c49da4fecdf9f52f10727d0ef7605785f
Daniel Karnopp Schuh <danielkchuh@gmail.com>
2025-06-04 07:37:59 -0300

    resolved 1.0

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ class Car:
 
 class CarWashStation:
     def __init__(
-            self, distance_from_city_center: bool, clean_power: int,
-            average_rating: bool , count_of_ratings: int
+            self, distance_from_city_center: float, clean_power: int,
+            average_rating: float , count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -26,16 +26,16 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def calculate_washing_price(self, car: str) -> None:
+    def calculate_washing_price(self, car: Car) -> None:
         cost = (car.comfort_class * (self.clean_power - car.clean_mark)
                 * self.average_rating / self.distance_from_city_center)
         return round(cost, 1)
 
-    def wash_single_car(self, car: str) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, new_rating: bool) -> None:
+    def rate_service(self, new_rating: float) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         total_rating += new_rating
         self.count_of_ratings += 1
