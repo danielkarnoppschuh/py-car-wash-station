3bd921a8f643c3a0b629fa2b75c1f92e65a4c5d3
Daniel Karnopp Schuh <danielkchuh@gmail.com>
2025-06-04 07:45:50 -0300

    resolved 1.2

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def calculate_washing_price(self, car: Car) -> None:
+    def calculate_washing_price(self, car: Car) -> float:
         cost = (car.comfort_class * (self.clean_power - car.clean_mark)
                 * self.average_rating / self.distance_from_city_center)
         return round(cost, 1)
